180a926dd4dd68f56b1a67557e96316f44cba538
zhufan <125920969@qq.com>
2019-03-11 15:33:12 +0800

    修正一下判断条件，防止正常情况下，会有判断错误问题

--- flexBox.js
+++ flexBox.js
@@ -1,9 +1,9 @@
 /**
  * 本方法用于计算页面根 rem 的大小
  */
-(function init () {
-  const flexBox = () => {
-    let fontSize = document.documentElement.clientWidth / 7.5
+(function init() {
+  var flexBox = function () {
+    var fontSize = document.documentElement.clientWidth / 7.5
     if (fontSize > 100) {
       fontSize = 100
     }
@@ -12,12 +12,15 @@
     var div = document.createElement('div')
     div.style = 'height:1rem;width:1rem'
     document.body.appendChild(div)
-    if (Math.abs(fontSize-div.offsetHeight)>1) {
-      fontSize = (fontSize * fontSize / div.offsetHeight)
+    if (Math.abs(fontSize - div.offsetHeight) > 2) {
+      fontSize = (fontSize * fontSize / div.offsetHeight);
+      if (fontSize > 100) {
+        fontSize = 100
+      }
+      document.documentElement.style.fontSize = `${fontSize}px`;
     }
-    document.body.removeChild(div)
-    document.documentElement.style.fontSize = `${fontSize}px`
-  }
+    document.body.removeChild(div);
+  };
   flexBox()
   window.addEventListener('resize', flexBox)
 }())
